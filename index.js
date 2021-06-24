8e5da38039fd7286d0fbfabdf26f0a22691f7c9d
niteesh <niteesh369@outlook.com>
2021-06-24 16:58:20 +0530

    final delete

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4">
             <div class="card">
               <div class="card-header d-flex justify-content-end gap-2">
                 <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-                <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+                <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclicl="deleteCard.apply(this, arguments)">
+                <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i></button>
               </div>
               <img src=${taskData.imageUrl} 
               class="card-img-top" alt="...">
@@ -58,3 +59,27 @@ const saveChanges = () => {
     localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
 };
 
+const deleteCard = (event) => {
+  event = window.event;
+  // id
+  const targetId = event.target.id;
+  const tagname = event.target.tagName; //BUTTON
+
+  // match the id of the element with the id inside the globalStore
+  // if match found remove
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetId);
+  localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+  // contact parent
+
+  if(tagname === "BUTTON"){
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }
+  else{
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+  
+
+
+};
